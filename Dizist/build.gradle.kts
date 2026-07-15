35ebe1ed4b5e61d028b354359ad9b40353eedc91
H.Akdeniz <akdeniz@HAkdeniz-MacBook-Air.local>
2026-07-15 13:20:58 +0300

    Add Dizist plugin (dizist.live)

---
+++ Dizist/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "Yabancı dizi, Asya dizileri, animeler ve Türkçe altyazılı/dublajlı en yeni dizi bölümleri"
+
+    status  = 1
+    tvTypes = listOf("TvSeries", "AsianDrama", "Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=https://dizist.live&sz=%size%"
+}
