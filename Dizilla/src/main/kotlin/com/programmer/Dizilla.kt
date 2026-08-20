e1388501efc72d323b40a04f459e2afebaaaa355
H.Akdeniz <akdeniz@HAkdeniz-MacBook-Air.local>
2026-08-20 22:49:47 +0300

    Update Dizilla mainUrl to dizilla.now

--- Dizilla/src/main/kotlin/com/programmer/Dizilla.kt
+++ Dizilla/src/main/kotlin/com/programmer/Dizilla.kt
@@ -17,7 +17,7 @@ import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 class Dizilla : MainAPI() {
-    override var mainUrl = "https://dizillahd.one"
+    override var mainUrl = "https://dizilla.now"
     override var name = "Dizilla"
     override val hasMainPage = true
     override var lang = "tr"
